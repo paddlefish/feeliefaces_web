662b0d1727bef5bbb06c9d0f7efd7ba707e4a2af
Andrew Rahn <andy@iconfactory.com>
2016-10-23 20:38:41 -0500

    Center grid and fix smushing on smaller devices

---
+++ feeliefaces.js
@@ -0,0 +1,24 @@
+// .modal-backdrop classes
+
+$(".modal-transparent").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  }, 0);
+});
+$(".modal-transparent").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+});
+
+$(".modal-fullscreen").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+  }, 0);
+});
+$(".modal-fullscreen").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
+
+$(document).ready( function() {
+  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+});
+
